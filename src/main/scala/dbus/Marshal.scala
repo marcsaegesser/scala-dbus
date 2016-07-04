c8426931bed7d3fd61703cd36ce8900cb4009772
Marc A. Saegesser <marc@saegesser.org>
2016-07-04 14:00:51 -0500

    Remove egregious seq.toList calls.

--- src/main/scala/dbus/Marshal.scala
+++ src/main/scala/dbus/Marshal.scala
@@ -11,8 +11,8 @@ import scodec.bits.{ ByteVector, BitVector, ByteOrdering }
 import DBus._
 
 trait Marshal {
-  def messageSignature(m: Seq[Field]): Throwable \/ Signature = (m.map(_.t)).toList.toSignature
-  def messageSignature_(m: Seq[Field]): Signature = (m.map(_.t)).toList.toSignature_
+  def messageSignature(m: Seq[Field]): Throwable \/ Signature = (m.map(_.t)).toSignature
+  def messageSignature_(m: Seq[Field]): Signature = (m.map(_.t)).toSignature_
 
   case class MarshalState(bits: BitVector, offset: Long)
 
