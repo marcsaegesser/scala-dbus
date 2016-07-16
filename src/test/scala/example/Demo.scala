d9a3830368c6817809a089796bfad98e55ebaa5a
Marc A. Saegesser <marc@saegesser.org>
2016-07-16 12:27:16 -0500

    Added Demo class.

---
+++ src/test/scala/example/Demo.scala
@@ -0,0 +1,17 @@
+package example
+
+import scala.io.StdIn
+import scalaz._,Scalaz._
+import dbus._,DBus._
+
+
+object Demo extends App {
+  val c = Connection.connect_(SessionBus)
+  c.requestName("example.echo", List())
+  implicit def exportEcho = ExportedObject.derive[Echo]
+  val echo = new Example()
+
+  c.export("/example/echo", echo)
+
+  val _ = StdIn.readLine("Press enter to exit...")
+}
